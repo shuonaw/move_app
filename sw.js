cd43d996328d42a0ea2aaa2c7b28db6ffbcc4494
shuonaw <shuonaw@gmail.com>
2026-08-09 19:58:51 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='move-v2';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
+const CACHE='move-v3';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/icon-192.png','/icon-512.png','/favicon-32.png','/favicon-16.png','/apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
